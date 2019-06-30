674575716d2aa0f9f51ff039070bba1054dec316
BrianPulfer <bp21developing@gmail.com>
2019-06-30 19:25:00 +0200

    Addition of the JDs and STs retrieval implementation. Configuration files missing (not yet working). Edit to the .gitignore (ignores .class files).

--- main/info_retrievers/JavaJDST/src/main/java/APIWrapper.java
+++ main/info_retrievers/JavaJDST/src/main/java/APIWrapper.java
@@ -1,25 +1,83 @@
+// Text Categorization 2011 imports
 import gov.nih.nlm.nls.tc.Api.JdiApi;
 import gov.nih.nlm.nls.tc.Api.StiApi;
+import gov.nih.nlm.nls.tc.FilterApi.InputFilterOption;
+import gov.nih.nlm.nls.tc.FilterApi.LegalWordsOption;
+import gov.nih.nlm.nls.tc.FilterApi.OutputFilter;
+import gov.nih.nlm.nls.tc.FilterApi.OutputFilterOption;
+import gov.nih.nlm.nls.tc.Lib.Configuration;
+import gov.nih.nlm.nls.tc.Lib.Count2f;
 
-import java.util.List;
+// JDK imports
+import java.util.*;
 
 
 public class APIWrapper {
     /**Wrapper class for the gov.nih.nlm.nlsc.tc API's (JournalDescriptors & SemanticTypes)*/
 
-    public APIWrapper(){
+    // JDs and STs API's
+    private JdiApi jdi;
+    private StiApi sti = new StiApi(new Configuration("data.Config.tc", true));
 
+    public APIWrapper(String tcPath){
+        Hashtable<String, String> properties = new Hashtable<String, String>();
+        properties.put("ROOT_DIR", tcPath);
+
+        this.jdi = new JdiApi(properties);
     }
 
-    public List<String> getJDs(String articleContent){
+    public List<String> getJDs(String text){
         /**Given a PubMed ID, returns the article's Journal Descriptors*/
-        // TODO
-        return null;
+        Vector<Count2f> scores = jdi.GetJdiScoresByTextMesh(text, new InputFilterOption(LegalWordsOption.DEFAULT_JDI));
+
+        OutputFilterOption outputFilterOption = new OutputFilterOption();
+        outputFilterOption.SetOutputNum(3);
+
+        String[] result = OutputFilter.ProcessText(scores, jdi.GetJournalDescriptors(), outputFilterOption).split("\n");
+
+        List<String> journalDescriptors = new ArrayList<String>();
+
+        if(result.length > 5) {
+            for (int i = 0; i < result.length; i++) {
+                if (i == 2 || i == 3 || i == 4) {
+                    String[] ttt = result[i].split("\\|");
+                    String blyad = ttt[3].trim().replace(",", ".");
+                    journalDescriptors.add(blyad);
+                }
+            }
+        }
+
+        //jdi.Close();
+        return journalDescriptors;
     }
 
-    public List<String> getSTs(String articleContent){
+    public List<String> getSTs(String text){
         /**Given a PubMed ID, returns the article's Semantic Types */
-        // TODO
-        return null;
+        Vector<Count2f> scores = sti.GetStiScoresByText(text, new InputFilterOption(LegalWordsOption.DEFAULT_JDI));
+
+        OutputFilterOption outputFilterOption = new OutputFilterOption();
+        outputFilterOption.SetOutputNum(3);
+
+        String[] result = OutputFilter.ProcessText(scores, sti.GetSemanticTypes(), outputFilterOption).split("\n");
+
+        List<String> semanticTypes = new ArrayList<String>();
+
+        if(result.length > 5){
+            for (int i = 0; i < result.length; i++){
+                if(i == 2 || i == 3 || i == 4){
+                    String[] ttt = result[i].split("\\|");
+                    String blyad = ttt[4].trim().replace(",", ".");
+                    semanticTypes.add(blyad);
+                }
+            }
+        }
+
+        //sti.Close();
+        return semanticTypes;
+    }
+
+    public void close(){
+        this.sti.Close();
+        this.jdi.Close();
     }
 }
--- main/info_retrievers/JavaJDST/src/main/java/JDSTServer.java
+++ main/info_retrievers/JavaJDST/src/main/java/JDSTServer.java
@@ -4,10 +4,12 @@ public class JDSTServer {
     /**Server class that will use the GatewayServer object to permit python code to access the JVM and an instance of
      * the APIWrapper*/
 
+    private static final String TC_PATH = "";
+
     public static void main(String[] args) {
         /**Creates and starts a GatewayServer involving an APIWrapper instance*/
 
-        GatewayServer server = new GatewayServer(new APIWrapper());
+        GatewayServer server = new GatewayServer(new APIWrapper(TC_PATH));
         System.out.println("Gateway server started");
         server.start();
     }
