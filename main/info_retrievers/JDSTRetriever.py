d61d60056e9fdf2ca262dae454b25f9579715c98
BrianPulfer <bp21developing@gmail.com>
2019-07-05 17:44:17 +0200

    Implementation of the python methods that extracts the JDs and STs from a string. Creation of the test of such class.

--- main/info_retrievers/JDSTRetriever.py
+++ main/info_retrievers/JDSTRetriever.py
@@ -1,19 +1,31 @@
+import os
+import time
+
 from py4j.java_gateway import JavaGateway
 
-# Connecting to the Java Gateway
-gateway = JavaGateway()
 
-# Saving a reference to the Java instance of 'TextCategorization'
-text_categorization = gateway.entry_point
-
-
-def get_jds(pmid):
-    """Given an article's PMID, returns the article Journal Descriptors"""
-    # TODO
-    pass
+def initialize_java_server():
+    java_class_path = os.path.dirname(os.path.abspath(__file__)) + "/JavaJDST/target/classes/JDSTServer"
+    command = "java "+java_class_path
+    os.system(command)
+    time.sleep(3)
 
 
-def get_sts(pmid):
-    """Given an article's PMID, returns the article Semantic Types"""
-    # TODO
-    pass
+class JDSTRetriever:
+
+    JAVA_SERVER_INITIALIZED = True
+
+    def __init__(self):
+        if not self.JAVA_SERVER_INITIALIZED:
+            initialize_java_server()
+            self.JAVA_SERVER_INITIALIZED = True
+
+        self._apiwrapper = JavaGateway().entry_point
+
+    def get_jds(self, text):
+        """Given an article's PMID, returns the article Journal Descriptors"""
+        return self._apiwrapper.getJDs(text)
+
+    def get_sts(self, text):
+        """Given an article's PMID, returns the article Semantic Types"""
+        return self._apiwrapper.getSTs(text)
