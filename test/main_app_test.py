83de58011fe3051ac2f1bd03e96161761a507eee
BrianPulfer <bp21developing@gmail.com>
2019-07-26 12:54:28 +0200

    Edit to the main_app_test so that it tests the 'fill_empty_with_average' function

--- test/main_app_test.py
+++ test/main_app_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from main import main_app
+from main.main_app import compute_accuracy, get_set, normalize_set, fill_empty_with_average
 from decimal import Decimal
 import pandas as pd
 import numpy as np
@@ -14,33 +14,33 @@ class MainTest(unittest.TestCase):
         # Testing full accuracy on 5 labels
         y_star = [1, 0, 0, 1, 0]
         y = [1, 0, 0, 1, 0]
-        self.assertEqual(main_app.compute_accuracy(y_star, y), 1,
+        self.assertEqual(compute_accuracy(y_star, y), 1,
                          "Accuracy should be 1 for [1,0,0,1,0] [1,0,0,1,0]")
 
         # Testing full failure on 1 label
         y_star = [1]
         y = [0]
-        self.assertEqual(main_app.compute_accuracy(y_star, y), 0,
+        self.assertEqual(compute_accuracy(y_star, y), 0,
                          "Accuracy should be 0 for [1] [0]")
 
         # Testing decimal accuracy on 5 labels
         y_star = [0, 0, 0, 0, 0]
         y = [0, 0, 0, 1, 1]
-        self.assertEqual(main_app.compute_accuracy(y_star, y), Decimal('0.6'),
+        self.assertEqual(compute_accuracy(y_star, y), Decimal('0.6'),
                          "Accuracy should be 0.6 for [0,0,0,0,0] [0,0,0,1,1]")
 
         # Testing decimal accuracy on 5 labels with different values
         y_star = [5.2, -10, -15.3, "str", 0]
         y = [5.2, -10, -15.3, "str", 1000]
-        self.assertEqual(main_app.compute_accuracy(y_star, y), Decimal('0.8'),
+        self.assertEqual(compute_accuracy(y_star, y), Decimal('0.8'),
                          "Accuracy should be 0.8 for [5.2, -10, -15.3, 'str', 0] [5.2, -10, -15.3, 'str', 1000]")
 
     def test_get_set(self):
         """Tests that data from CSV file is loaded without null values"""
 
         # Retrieving sets
-        training_set = main_app.get_set('../dataset/1500_pairs_train.csv')
-        testing_set = main_app.get_set('../dataset/400_pairs_test.csv')
+        training_set = get_set('../dataset/1500_pairs_train.csv')
+        testing_set = get_set('../dataset/400_pairs_test.csv')
 
         sets = [training_set, testing_set]
 
@@ -66,14 +66,29 @@ class MainTest(unittest.TestCase):
         new_col3 = (dataset[:, 3] - col3_mean) / col3_std
 
         new_set = [new_col0, new_col1, new_col2, new_col3]
-        normalized_set = main_app.normalize_set(dataset, [False, False, False, False])
+        normalized_set = normalize_set(dataset, [False, False, False, False])
 
         for col in range(len(normalized_set[0])):
             for row in range(len(normalized_set)):
                 self.assertEqual(new_set[col][row], normalized_set[row, col])
 
     def test_fill_empty_with_average(self):
-        pass
+        """Tests that a bi-dimensional matrix containing '-1' values is replaced with the same matrix with mean column
+         value instead of the '-1's."""
+        test_matrix = np.array([[1, 2, 3],
+                                [1, -1, 5],
+                                [-1, -1, -1]])
+
+        expected = np.array([[1, 2, 3],
+                             [1, 2, 5],
+                             [1, 2, 4]])
+
+        new_matrix = fill_empty_with_average(test_matrix)
+
+        for row in range(len(expected)):
+            for col in range(len(expected[row])):
+                self.assertEqual(expected[row, col], new_matrix[row, col])
+
 
 if __name__ == '__main__':
     unittest.main()
