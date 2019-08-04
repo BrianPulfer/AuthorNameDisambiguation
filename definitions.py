63a7885e4d4c1be4951c498d40e407544e20d8c2
Brian Pulfer <brian.pulfer@student.supsi.ch>
2019-08-04 22:24:42 +0200

    Addition of the training and testing set PATHS to the definitions

--- definitions.py
+++ definitions.py
@@ -1,3 +1,5 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+PATH_TO_TRAINING_SET = ROOT_DIR + '/dataset/1500_pairs_train.csv'
+PATH_TO_TESTING_SET = ROOT_DIR + '/dataset/400_pairs_test.csv'
