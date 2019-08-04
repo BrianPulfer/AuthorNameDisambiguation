df994f29b6de51e82fad3c998c0061e0d89f87ca
Brian Pulfer <brian.pulfer@student.supsi.ch>
2019-08-05 00:29:07 +0200

    Addition of test files for the SVM and CART classifiers

---
+++ test/classifiers_test/cart_test.py
@@ -0,0 +1,22 @@
+import unittest
+from main.classifiers.cart import CART
+
+
+class TestCART(unittest.TestCase):
+    def test_classifier(self):
+        """Tests the general behaviour of the classifier (train and predict)"""
+        x_train = [[0, 0], [0, 1], [1, 0], [1, 1]]
+        y_train = [0, 1, 1, 1]
+
+        classifier = CART()
+        classifier.fit(x_train, y_train)
+
+        prediction_1 = classifier.predict([[0, 0]])
+        prediction_2 = classifier.predict([[1, 1]])
+
+        self.assertEqual(0, prediction_1)
+        self.assertEqual(1, prediction_2)
+
+
+if __name__ == '__main__':
+    unittest.main()
