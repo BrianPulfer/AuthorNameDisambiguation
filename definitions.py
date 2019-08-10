5511f829fabd1f4221797c2c8847dc05e0bfe3a8
Brian Pulfer <brian.pulfer@student.supsi.ch>
2019-08-10 09:27:52 +0200

    Addition of PATHS in the definitions script

--- definitions.py
+++ definitions.py
@@ -1,6 +1,10 @@
 import os
 
+# PATHS
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_TRAINING_SET = ROOT_DIR + '/dataset/1500_pairs_train.csv'
 PATH_TO_TESTING_SET = ROOT_DIR + '/dataset/400_pairs_test.csv'
 PATH_TO_ARTICLES = ROOT_DIR + '/dataset/articles/'
+PATH_TO_ENTITIES = ROOT_DIR + '/dataset/articles_entities/'
+PATH_TO_JDS_STS = ROOT_DIR + '/dataset/articles_jdst/'
+PATH_TO_LOC_ORGS = ROOT_DIR + '/dataset/articles_locs_orgs/'
