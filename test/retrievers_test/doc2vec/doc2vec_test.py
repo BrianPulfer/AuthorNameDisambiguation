335ad289b63fd4ee3d89cfefd76c5d0362ad2796
Brian Pulfer <brian.pulfer@student.supsi.ch>
2019-08-13 02:19:14 +0200

    Edit to the Doc2Vec test to make it execute within 10 seconds.

--- test/retrievers_test/doc2vec/doc2vec_test.py
+++ test/retrievers_test/doc2vec/doc2vec_test.py
@@ -9,12 +9,25 @@ class TestDoc2Vec(unittest.TestCase):
     def test_vector(self):
         """Tests that a vector is retrieved correctly for every article"""
 
-        for filename in os.listdir(definitions.PATH_TO_ARTICLES):
-            if filename.endswith('.xml'):
-                pmid = filename[:-4]
+        # Only tests one file every 'rate'.
+        rate = 100
+        rate_count = 0
+        tested = 0
 
-                vector = doc2vec.get_vector(pmid)
-                self.assertEqual(doc2vec.DOC2VEC_VECTOR_DIMENSION, len(vector))
+        for filename in os.listdir(definitions.PATH_TO_ARTICLES):
+            rate_count = rate_count + 1
+
+            if rate_count == rate:
+                rate_count = 0
+                if filename.endswith('.xml'):
+                    pmid = filename[:-4]
+
+                    vector = doc2vec.get_vector(pmid)
+                    self.assertEqual(doc2vec.DOC2VEC_VECTOR_DIMENSION, len(vector))
+                    tested = tested + 1
+
+        # Checks that at least 10 files were tested
+        self.assertTrue(tested > 10)
 
 
 if __name__ == '__main__':
